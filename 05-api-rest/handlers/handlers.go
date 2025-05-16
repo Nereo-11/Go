abde5ea72e7c61c8578ac05fd92391561ea20858
NereoUriel <115206702+Nereo-11@users.noreply.github.com>
2025-05-15 21:29:47 -0600

    Actualización Go

--- 05-api-rest/handlers/handlers.go
+++ 05-api-rest/handlers/handlers.go
@@ -1,10 +1,11 @@
 package handlers
 
 import (
-	"apirest/db"
+	//	"apirest/db"
 	"apirest/models"
 	"encoding/json"
-	"fmt"
+
+	//	"fmt"
 	"net/http"
 	"strconv"
 
@@ -39,51 +40,45 @@ func CreateUser(rw http.ResponseWriter, r *http.Request) {
 	decoder := json.NewDecoder(r.Body)
 
 	if err := decoder.Decode(&user); err != nil {
-		fmt.Fprintln(rw, http.StatusUnprocessableEntity)
+		models.SendUnprocessableEntity(rw)
 	} else {
-		db.Connect()
 		user.Save()
-		db.Close()
+		models.SendData(rw, user)
 	}
 
-	output, _ := json.Marshal(user)
-	fmt.Fprintln(rw, string(output))
 }
 
 func UpdateUser(rw http.ResponseWriter, r *http.Request) {
-	rw.Header().Set("Content-Type", "application/json")
 
 	//Obtener registro
-	user := models.User{}
+	var userId int64
+	if user, err := getUserByRequest(r); err != nil {
+		models.SendNoFound(rw)
+	} else {
+		userId = user.Id
+	}
 
+	user := models.User{}
 	decoder := json.NewDecoder(r.Body)
 
 	if err := decoder.Decode(&user); err != nil {
-		fmt.Fprintln(rw, http.StatusUnprocessableEntity)
+		models.SendUnprocessableEntity(rw)
 	} else {
-		db.Connect()
+		user.Id = userId
 		user.Save()
-		db.Close()
+		models.SendData(rw, user)
 	}
 
-	output, _ := json.Marshal(user)
-	fmt.Fprintln(rw, string(output))
 }
 
 func DeleteUser(rw http.ResponseWriter, r *http.Request) {
-	rw.Header().Set("Content-Type", "application/json")
 
-	//Obtener id
-	vars := mux.Vars(r)
-	userId, _ := strconv.Atoi(vars["id"])
-
-	db.Connect()
-	user, _ := models.GetUser(userId)
-	user.Delete()
-	db.Close()
-
-	output, _ := json.Marshal(user)
-	fmt.Fprintln(rw, string(output))
+	if user, err := getUserByRequest(r); err != nil {
+		models.SendNoFound(rw)
+	} else {
+		user.Delete()
+		models.SendData(rw, user)
+	}
 }
 
 func getUserByRequest(r *http.Request) (models.User, error) {
